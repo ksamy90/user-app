98d0d481aa1e919a38e30f1b314dcd7fd0746b44
samson <samson.omondi1@ibm.com>
2022-12-31 16:45:12 +0300

    fetch data from localstorage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import UserForm from "./components/UserForm";
 import UsersList from "./components/UsersList";
 import styles from "./css/App.module.css";
 
 function App() {
   const [users, setUsers] = useState([]);
+
   const addUsers = (user) => {
     setUsers([...users, user]);
   };
+  const fetchUsers = localStorage.getItem("users");
+
+  useEffect(() => {
+    const updatedUsers = JSON.parse(fetchUsers);
+    setUsers(updatedUsers);
+  }, [fetchUsers]);
+
+  useEffect(() => {
+    const data = JSON.stringify(users);
+    if (users.length > 0) {
+      localStorage.setItem("users", data);
+    }
+  }, [users]);
 
   return (
     <div>
